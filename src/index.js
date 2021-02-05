3eaf6c4269764fd510a34ea81d85d98922021bff
兰世豪 <1183643226@qq.com>
2021-02-05 14:00:07 +0800

    通过context传递store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import store from './store';
 
+import {StoreContext} from'./utils/context'
 
-ReactDOM.render(<App />,document.getElementById('root'));
+ReactDOM.render(
+  <StoreContext.Provider value={store}>
+    <App />
+  </StoreContext.Provider>
+  , document.getElementById('root')
+);
 
--- src/utils/connect.js
+++ src/utils/connect.js
@@ -1,19 +1,20 @@
 import { PureComponent } from "react";
-import store from "../store";
+// import store from "../store";
+import { StoreContext } from "./context";
 
 export function connect(propsState,propsDispatch){
   return function changeHOC(Hoc){
     class EnhanceComponent extends PureComponent {
-      constructor(){
-        super();
+      constructor(props,context){
+        super(props,context);
         this.state = {
-          connectState : propsState(store.getState())
+          connectState : propsState(context.getState())
         }
       }
       componentDidMount(){
-        store.subscribe(()=>{
+        this.context.subscribe(()=>{
           this.setState({
-            connectState:propsState(store.getState())
+            connectState:propsState(this.context.getState())
           })
         })
       }
@@ -21,12 +22,14 @@ export function connect(propsState,propsDispatch){
       render(){
         return (
           <Hoc state={this.state}
-               {...propsState(store.getState())}
-               {...propsDispatch(store.dispatch)}
+               {...propsState(this.context.getState())}
+               {...propsDispatch(this.context.dispatch)}
           />
         )
       }
     }
+    EnhanceComponent.contextType = StoreContext;
+
     return EnhanceComponent;
   }
 }
