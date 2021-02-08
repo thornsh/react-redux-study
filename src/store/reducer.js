7972c14a4079fa4222a3a88dc339f7dddf983e83
兰世豪 <1183643226@qq.com>
2021-02-08 10:18:23 +0800

    reducer合并的基本实现

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,15 +2,37 @@ const defaultState = {
   counter:0
 }
 
-export default function reducer(state = defaultState,action){
-  switch (action.type) {
-    case "ADD_NUM":
-      return{...state,counter:state.counter + action.num};
-    case "SUB_NUM":
-      return{...state,counter:state.counter - action.num};
-    case "THIRD_NUM":
-      return {...state,counter:state.counter + action.num*2};
-    default:
-      return state;
+// export default function reducer(state = defaultState,action){
+//   switch (action.type) {
+//     case "ADD_NUM":
+//       return{...state,counter:state.counter + action.num};
+//     case "SUB_NUM":
+//       return{...state,counter:state.counter - action.num};
+//     case "THIRD_NUM":
+//       return {...state,counter:state.counter + action.num*2};
+//     default:
+//       return state;
+//   }
+// }
+function reducer(state,action){
+  return {
+    counter: counter(state, action)
   }
 }
+
+function counter(state, action){
+  switch (action.type) {
+    case "ADD_NUM":
+      return state.counter + action.num
+  
+    default:
+      break;
+  }
+}
+
+let aaa = reducer(defaultState,{
+  type:"ADD_NUM",
+  num:1
+})
+console.log(aaa);
+
